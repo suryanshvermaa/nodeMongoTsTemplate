160a992c6b7b07b158d7dc306c40d6ef990706c1
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-05-25 11:06:00 +0530

    added timestamp to usermodel

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -21,7 +21,7 @@ const UserSchema: Schema<IUser> = new Schema({
 		required: true,
 		unique: true,
 	},
-});
+},{timestamps:true});
 
 const User = mongoose.model("User", UserSchema);
 export default User;
