8667ccf99851132adbcbe66e818d7e30de267ee3
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-06-15 00:04:00 +0530

    aws s3 added

---
+++ src/config/s3.ts
@@ -0,0 +1,14 @@
+import { S3Client } from "@aws-sdk/client-s3";
+import "dotenv/config";
+
+const s3client = new S3Client({
+	region: process.env.S3_REGION!,
+	endpoint: process.env.S3_ENDPOINT!, //if using s3 like service,otherwise remove this property
+	credentials: {
+		accessKeyId: process.env.ACCESS_KEY_ID!,
+		secretAccessKey: process.env.SECRET_ACCESS_KEY!,
+	},
+	forcePathStyle: true,// if using s3 like service,otherwise remove this property
+});
+
+export default s3client;
